cf51b8e317d54eac943f312939bba5df9e001f80
shweta.chaudhary <shweta.chaudhary@kellton.com>
2023-06-01 13:48:10 +0530

    commiting

--- onlineCodingPlateform/src/app/app-routing.module.ts
+++ onlineCodingPlateform/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',pathMatch:"full",redirectTo:'login'},
+  {path:'login',component : LoginComponent},
+  {path:'create-account',component : CreateAccountComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- onlineCodingPlateform/src/app/app.module.ts
+++ onlineCodingPlateform/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './pages/login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
